40bab2a33daeacbfc36834285a12d9dfb0d96ab9
Jozef Cipa <cipa.jozef@gmail.com>
2025-03-13 09:15:06 +0100

    feat: update vfd

--- main/vfd.c
+++ main/vfd.c
@@ -1,11 +1,14 @@
 #include "vfd.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+static TaskHandle_t scroll_text_task_handle = NULL;
 
 static void spi_write(uint8_t w_data) {
   for (uint8_t i = 0; i < 8; i++) {
@@ -32,6 +35,47 @@ static void vfd_init_reset() {
   gpio_set_level(PIN_NUM_RESET, 1);
 }
 
+static void _vfd_write(uint8_t x, const char *str, bool cancel_task) {
+  // cancel the ongoing task first if enabled
+  if (cancel_task && scroll_text_task_handle != NULL) {
+    vTaskSuspend(scroll_text_task_handle);
+  }
+
+  gpio_set_level(PIN_NUM_CS, 0);
+  spi_write(VFD_DISPLAY_START_ADDR + x);
+  while (*str) {
+    spi_write(*str);
+    str++;
+  }
+  gpio_set_level(PIN_NUM_CS, 1);
+
+  vfd_cmd(VFD_CMD_PRINT);
+}
+
+static void task_scroll_text(void *params) {
+  const char *str = (char *)params;
+  int str_len = strlen(str);
+  int padded_len = str_len + 2;  // Add 2 spaces for smoother transition
+
+  while (1) {
+    for (int i = 0; i < padded_len; i++) {
+      char display_text[DISPLAY_NUM_CHARS + 1];
+      int j;
+
+      for (j = 0; j < DISPLAY_NUM_CHARS; j++) {
+        int index = (i + j) % padded_len;
+
+        // Append spaces at the end before looping back
+        display_text[j] = (index < str_len) ? str[index] : ' ';
+      }
+      display_text[j] = '\0';
+
+      _vfd_write(0, display_text, false);
+      vTaskDelay(pdMS_TO_TICKS(200));
+    }
+  }
+}
+
 void vfd_cmd(uint8_t cmd) {
   gpio_set_level(PIN_NUM_CS, 0);
   spi_write(cmd);
@@ -49,38 +93,20 @@ void vfd_init() {
       .intr_type = GPIO_INTR_DISABLE};
   gpio_config(&conf);
 
-  vfd_init_reset(); // Init VFD display by sending a RESET signal
-  vfd_config(VFD_CONF_DISPLAY_SIZE, 0x07); // 8 digits
-  vfd_config(VFD_CONF_BRIGHTNESS, 0xFF); // min 0x01 - max 0xFF
+  vfd_init_reset();                         // Init VFD display by sending a RESET signal
+  vfd_config(VFD_CONF_DISPLAY_SIZE, 0x07);  // 8 digits
+  vfd_config(VFD_CONF_BRIGHTNESS, 0xFF);    // min 0x01 - max 0xFF
 }
 
 void vfd_write(uint8_t x, const char *str) {
-  gpio_set_level(PIN_NUM_CS, 0);
-  spi_write(VFD_DISPLAY_START_ADDR + x);
-  while (*str) {
-    spi_write(*str);
-    str++;
-  }
-  gpio_set_level(PIN_NUM_CS, 1);
-
-  vfd_cmd(VFD_CMD_PRINT);
+  _vfd_write(x, str, true);
 }
 
 void vfd_write_scroll(const char *str) {
-  int str_len = strlen(str);
-
-  while (1) {
-    for (int i = 0; i < str_len; i++) {
-      char display_text[DISPLAY_NUM_CHARS + 1];
-      int j;
-
-      for (j = 0; j < DISPLAY_NUM_CHARS; j++) {
-        display_text[j] = (i + j < str_len) ? str[i + j] : ' ';
-      }
-      display_text[j] = '\0';
-
-      vfd_write(0, display_text);
-      vTaskDelay(pdMS_TO_TICKS(150));
-    }
+  // cancel the ongoing task first
+  if (scroll_text_task_handle != NULL) {
+    vTaskSuspend(scroll_text_task_handle);
   }
+
+  xTaskCreate(task_scroll_text, "ScrollTextTask", configMINIMAL_STACK_SIZE * 3, str, 5, &scroll_text_task_handle);
 }
